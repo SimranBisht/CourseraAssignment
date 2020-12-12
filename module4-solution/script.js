87ee5057b1c50ff850b6fcea06f6c14293acc598
Simran Bisht <simranbisht2006@gmail.com>
2020-12-12 13:02:42 +0530

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,9 @@
+var arr=['Mira','Jaspreet','Shiela','Jayant','Prachi'];
+for (var i=0;i<arr.length;i++){
+  if (arr[i].charAt(0)=="J"){
+    console.log("Good Bye "+arr[i]);
+  }
+  else{
+  console.log("Hello "+arr[i]);  
+  }
+}
